6e83bbf92d6db24147f00cbb53bced348580e520
Thomas Durand <thomas.durand.verjat@gmail.com>
2019-04-05 16:48:45 +0200

    Initial Commit

---
+++ ImageUtility/ImageUtility.h
@@ -0,0 +1,19 @@
+//
+//  ImageUtility.h
+//  ImageUtility
+//
+//  Created by Thomas DURAND on 05/04/2019.
+//  Copyright © 2019 Thomas DURAND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageUtility.
+FOUNDATION_EXPORT double ImageUtilityVersionNumber;
+
+//! Project version string for ImageUtility.
+FOUNDATION_EXPORT const unsigned char ImageUtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageUtility/PublicHeader.h>
+
+
