788b6403488e2aa633a01ae2d8065a08c25d3fd9
Thomas Durand <thomas.durand.verjat@gmail.com>
2019-04-05 17:11:49 +0200

    Build settings & deployment targets

--- Tests/ImageUtilityTests.swift
+++ Tests/ImageUtilityTests.swift
@@ -1,6 +1,6 @@
 /*
- * Created by Thomas DURAND on 05/04/2019.
- * Copyright © 2019 Thomas DURAND. All rights reserved.
+ * ImageUtilityTests.swift
+ * Created by Thomas DURAND
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
